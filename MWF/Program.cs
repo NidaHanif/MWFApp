60df426aba5500a67d244fcbcccdab19549e2d97
nhanif220@gmail.com <nhanif220@gmail.com>
2024-09-11 21:38:12 +0500

    11-9-2024 inputs

--- MWF/Program.cs
+++ MWF/Program.cs
@@ -8,8 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddDbContext<DonationDbContext>(options =>
-    options.UseSqlite("Data Source=Recieptt.db"));
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
